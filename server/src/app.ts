8f55b16e8396eb32baea6b91d6ab41c929b9f221
Davide Russo <davide.russo@rina.org>
2021-04-05 22:31:33 +0200

    feat(logger): switch winston for morgan logger, change some tslint rules

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,17 @@
-import {Express} from 'express';
 import {MONGO_URI, SERVER_HOST, SERVER_PORT} from "./config/server.config";
 import {postsRouter} from "./api/posts.route";
-import {baseLogsConfig, errorsLogsConfig} from "./config/logger.config";
-import * as expressWinston from 'express-winston';
 
 const express = require('express');
-const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
-const app: Express = express();
+const app = express();
+
+app.use(morgan('combined'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
-app.use(cors());
-app.use(expressWinston.logger(baseLogsConfig));
 app.use('/posts', postsRouter);
-app.use(expressWinston.errorLogger(errorsLogsConfig));
 
-const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 mongoose.connect(`${MONGO_URI}/posts`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(SERVER_PORT, SERVER_HOST, () => console.log(`Posts: http://${SERVER_HOST}:${SERVER_PORT}\n`)))
